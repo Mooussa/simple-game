90d3e1c73ec2c92330eaa0cc13a641d9435160e9
Your Name <you@example.com>
2022-11-02 16:56:34 +0000

    First commit

---
+++ quiz-game.py
@@ -0,0 +1,52 @@
+#Quiz Game in Python
+
+print("Welcome to my Computer Game!!")
+
+
+
+playing = input("Do you want to play?: ").lower()
+
+if playing != "yes":
+    quit()
+while True:
+    print("Okay! Let's play :")
+    score = 0
+
+    answer = input("What does a CPU stand for? ").lower()
+    if answer == "central processing unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    answer = input("What does a GPU stand for? ").lower()
+    if answer == "graphic processing unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+
+    answer = input("What does a TPU stand for? ").lower()
+    if answer == "tensor processing unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+
+    answer = input("What does a IPU stand for? ").lower()
+    if answer == "infrastructure processing unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    print("You got " + str(score) + " questions correcct!")
+    print("You got " + str((score/4)*100) + "%")
+
+    play_again = input("Do you want to play again? (yes/no)").lower()
+
+    if play_again != "yes":
+        break
+print('Bye!!')
